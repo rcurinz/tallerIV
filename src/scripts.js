50ec466c360ddf27cee5e688e23e038c70f679fe
Rene <rcurin2013@alu.uct.cl>
2019-08-30 22:36:38 -0400

    gitflow-release-stash: v1

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,15 @@
 const app = new Vue({
     el: '#app',
     data:{
+        //Aqui van las variables (ej: contador=0, iniciar=False, nombres = [], etc)
 
     },
     methods:{
+        //Aqui van los metodos o funciones (se escriben nombre_metodo(){}, )
         
+    },
+    computed:{
+
     }
 
 })
