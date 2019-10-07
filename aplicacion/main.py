2ac262c40d7ad3ca410e0e932b658c2d88aeda4a
Lesly <lesly1994@gmail.com>
2019-10-07 00:28:37 -0400

    conexion a base de datos

---
+++ aplicacion/main.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+from .extensions import mongo 
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    user_collection = mongo.db.medico
+    user_collection.insert({'name' : 'rosa'})
+    user_collection.insert({'name' : 'pepe'})
+    return '<h1>Added a User!</h1>'
+
+"""
+ tutorial:
+
+https://www.youtube.com/watch?v=3ZS7LEH_XBg 
+"""
